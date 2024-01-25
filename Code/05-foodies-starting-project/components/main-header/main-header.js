59b19776691377e931481d5e2f7ba71b95037b0b
jjjjjeongwon <jjjjjwon21@gmail.com>
2024-01-25 23:48:30 +0900

    complete

--- Code/05-foodies-starting-project/components/main-header/main-header.js
+++ Code/05-foodies-starting-project/components/main-header/main-header.js
@@ -4,9 +4,10 @@ import Image from "next/image";
 import MainHeaderBackGround from "./main-header-background";
 import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
+import { usePathname } from "next/navigation";
+import NavLink from "./nav-link";
 
 export default function MainHeader() {
-  console.log("Executing");
   return (
     <>
       <MainHeaderBackGround />
@@ -19,10 +20,10 @@ export default function MainHeader() {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Browse Meals</Link>
+              <NavLink href="/meals">Browse Meals</NavLink>
             </li>
             <li>
-              <Link href="/community">Foodies Community</Link>
+              <NavLink href="/community">Foodies Community</NavLink>
             </li>
           </ul>
         </nav>
