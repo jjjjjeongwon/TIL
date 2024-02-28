e4d4d29a199a2967c683bc36512bed691b7b82be
jjjjjeongwon <jjjjjwon21@gmail.com>
2024-02-28 10:41:16 +0900

    FEAT: 124-complete

--- Code/05-foodies-starting-project/lib/action.js
+++ Code/05-foodies-starting-project/lib/action.js
@@ -3,6 +3,10 @@
 import { redirect } from "next/navigation";
 import { saveMeal } from "./meals";
 
+function isInvaildText(text) {
+  return !text || text.trim() === "";
+}
+
 export async function shareMeal(formData) {
   const meal = {
     title: formData.get("title"),
@@ -12,6 +16,19 @@ export async function shareMeal(formData) {
     creator: formData.get("name"),
     creator_email: formData.get("email"),
   };
+
+  if (
+    isInvaildText(meal.title) ||
+    isInvaildText(meal.summary) ||
+    isInvaildText(meal.instructions) ||
+    isInvaildText(meal.creator) ||
+    isInvaildText(meal.creator_email) ||
+    !meal.creator_email.includes("@") ||
+    !meal.image ||
+    meal.image.size === 0
+  ) {
+    throw new Error("Invaild input");
+  }
 
   await saveMeal(meal);
   redirect("/meals");
