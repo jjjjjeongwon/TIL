d6502142ee088802e03f1804889d03791e5023c7
jjjjjeongwon <jjjjjwon21@gmail.com>
2024-04-21 23:46:46 +0900

    FEAT: 179-complete

--- Code/06/nextjs-course-code/pages/index.js
+++ Code/06/nextjs-course-code/pages/index.js
@@ -13,6 +13,7 @@ function HomePage(props) {
 }
 
 export async function getStaticProps() {
+  console.log("(Re-)Generating...");
   const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
