fc1b5f86266cfd5f35d76f5d3ecb49ea0d2bc317
jjjjjeongwon <jjjjjwon21@gmail.com>
2024-04-28 23:10:23 +0900

    FEAT:185-complete

--- Code/06/nextjs-course-code/pages/[pid].js
+++ Code/06/nextjs-course-code/pages/[pid].js
@@ -5,6 +5,10 @@ import { Fragment } from "react";
 
 function ProductDetailPage(props) {
   const { loadedProduct } = props;
+
+  //   if (!loadedProduct) {
+  //     return <p>Loading...</p>;
+  //   }
   return (
     <Fragment>
       <h1>{loadedProduct.title}</h1>
@@ -33,12 +37,8 @@ export async function getStaticProps(context) {
 
 export async function getStaticPaths() {
   return {
-    paths: [
-      { params: { pid: "p1" } },
-      { params: { pid: "p2" } },
-      { params: { pid: "p3" } },
-    ],
-    fallback: false,
+    paths: [{ params: { pid: "p1" } }],
+    fallback: "blocking",
   };
 }
 
